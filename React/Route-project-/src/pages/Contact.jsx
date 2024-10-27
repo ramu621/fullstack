e6f252bd63838a5a118201e4a77e75507ebb250d
ramu621 <bollepalliramu123@gmail.com>
2024-10-27 17:32:10 +0530

    day 6.2

---
+++ React/Route-project-/src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+      <p>prime minsiter</p>
+    </>
+  )
+}
+
+export default Contact
---
+++ React/Route-project-/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <p>This is Home Page</p>
+    </>
+  )
+}
+
+export default Home
